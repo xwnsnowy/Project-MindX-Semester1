4580ee3ffc1892265e683e91350e4f7d393e7f88
Nguyen Tien Thanh <thanhnthe163528@fpt.edu.vn>
2023-11-02 09:27:21 +0700

    transition js when website first loaded

--- ProjectMindX/assets/js/main.js
+++ ProjectMindX/assets/js/main.js
@@ -10,6 +10,28 @@
 //     document.getElementById("footer").innerHTML = data;
 //   });
 // Scroll Header
+document.addEventListener("DOMContentLoaded", function () {
+  const h1HomeGetStarted = document.querySelector(
+    "#home .container-home .home-get-started h1"
+  );
+  h1HomeGetStarted.style.opacity = 1;
+  h1HomeGetStarted.style.transform = "translateY(0)";
+
+  const spanHomeGetStarted = document.querySelector(
+    "#home .container-home .home-get-started span"
+  );
+  spanHomeGetStarted.style.opacity = 1;
+  spanHomeGetStarted.style.transform = "translateY(0)";
+
+  const getStartedHome = document.querySelector(".get-started-home");
+  getStartedHome.style.opacity = 1;
+  getStartedHome.style.transform = "translateY(0)";
+
+  const imgHome = document.querySelector(".home-img");
+  imgHome.style.opacity = 1;
+  imgHome.style.transform = "translateY(0)";
+});
+
 window.addEventListener("scroll", function () {
   let header = document.querySelector(".scroll-header");
   let scrollPosition = window.scrollY;
@@ -19,7 +41,6 @@ window.addEventListener("scroll", function () {
     header.classList.remove("header-background");
   }
 });
-
 // Header toggle
 
 // Typing Effect
