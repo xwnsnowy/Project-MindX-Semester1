8bde2153c93d2a55f270729d29383876c46d0983
Nguyen Tien Thanh <thanhnthe163528@fpt.edu.vn>
2023-11-04 08:55:21 +0700

    typing effect for screen home, fix body home in screen maxwidth500px

--- ProjectMindX/assets/js/main.js
+++ ProjectMindX/assets/js/main.js
@@ -16,10 +16,18 @@
 
 // Typing Effect
 
-// var typed = new Typed('.auto-input', {
-//   strings: ['Front-End Developer!', 'Freelancer!', 'UI Designer'],
-//   typeSpeed: 100,
-//   backSpeed: 100,
-//   backDelay: 2000,
-//   loop: true,
-// });
+const text = document.querySelector(".typing-span-home");
+
+const textTyping = () => {
+  setTimeout(() => {
+    text.textContent = "Bootstrap";
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "for your success";
+  }, 4000);
+  setTimeout(() => {
+    text.textContent = "for excellence";
+  }, 8000);
+};
+textTyping();
+setInterval(textTyping, 12000);
